e5851d125479cf153b91218da72afd6cea584a8e
iva mudronja <iva.pilizota@gmail.com>
2021-01-04 16:28:46 +0100

    improved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,10 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params[:question].downcase == "I am going to work"
-    	@answer = "Great!"
-    elsif params[:question].end_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
+    if params[:question].blank?
+      @answer = "I can't hear you!"
+    elsif params[:question].downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
